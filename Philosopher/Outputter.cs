5f0aeaf59a365086142852ba38e40b7bda3d69e5
PAUL CHEN <pchenatwork@hotmail.com>
2019-05-17 16:04:35 -0400

    File structure refactor

---
+++ Philosopher/Outputter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Philosopher
+{
+    /// <summary>
+    /// OutputterBase implemented lock to make it threadsafe
+    /// </summary>
+    public abstract class AbstractOutputter : IOutputter
+    {
+        private readonly object _locker = new object();
+        public void WriteLine(string s)
+        {  ///** make sure one thread user "WriteLine" at a time**
+            lock (_locker)
+            {
+                _WriteLine(s);
+            }
+        }
+        protected abstract void _WriteLine(string s);
+    }
+}
